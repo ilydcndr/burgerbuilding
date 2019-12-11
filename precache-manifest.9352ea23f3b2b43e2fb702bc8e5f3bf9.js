5c4dc328fffdb9a6924a67c115d010b561e0174f
ilydcndr <ilaydacundar@hotmail.com>
2019-12-11 16:34:44 +0200

    Updates

---
+++ precache-manifest.9352ea23f3b2b43e2fb702bc8e5f3bf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fe69dd3778d8333f20c4caf6c8de4c4",
+    "url": "/burgerbuilding/index.html"
+  },
+  {
+    "revision": "847571550e5ab65dbdff",
+    "url": "/burgerbuilding/static/css/main.ca0207c2.chunk.css"
+  },
+  {
+    "revision": "aae772671dfbbefac188",
+    "url": "/burgerbuilding/static/js/2.cb67f085.chunk.js"
+  },
+  {
+    "revision": "e92e356b467ce5268290e03394dbf217",
+    "url": "/burgerbuilding/static/js/2.cb67f085.chunk.js.LICENSE"
+  },
+  {
+    "revision": "847571550e5ab65dbdff",
+    "url": "/burgerbuilding/static/js/main.53b57d88.chunk.js"
+  },
+  {
+    "revision": "a1efcc35e8a7945505df",
+    "url": "/burgerbuilding/static/js/runtime-main.b0c57cd2.js"
+  }
+]);
